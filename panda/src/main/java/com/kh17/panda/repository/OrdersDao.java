baee924b9bd48735409e6540f35a364360871b5e
kimjiwon <kjw0112m@naver.com>
2019-08-22 16:46:35 +0900

    190822 master

--- panda/src/main/java/com/kh17/panda/repository/OrdersDao.java
+++ panda/src/main/java/com/kh17/panda/repository/OrdersDao.java
@@ -1,5 +1,5 @@
 package com.kh17.panda.repository;
 
 public interface OrdersDao {
-	
+	void initSequnce();
 }
--- panda/src/main/java/com/kh17/panda/service/SequenceInitTaskService.java
+++ panda/src/main/java/com/kh17/panda/service/SequenceInitTaskService.java
@@ -3,10 +3,8 @@ package com.kh17.panda.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface ScheduleService {
-	void productSeqInit();
-	void test();
-	void work1();
+public interface SequenceInitTaskService {
+	void orderSeqInit();
 }
 
 
