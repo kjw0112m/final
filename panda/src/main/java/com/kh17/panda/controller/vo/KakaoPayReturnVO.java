488bc8cdf1439d0ef24899a2842672ff356aa114
hal0219 <hal0219@naver.com>
2019-08-23 18:51:16 +0900

    20190823 jimin only commit

---
+++ panda/src/main/java/com/kh17/panda/controller/vo/KakaoPayReturnVO.java
@@ -0,0 +1,13 @@
+package com.kh17.panda.controller.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//카카오 페이 요청 시 반환 자료형
+@Data @Builder @NoArgsConstructor @AllArgsConstructor
+public class KakaoPayReturnVO {
+	private String tid, next_redirect_pc_url, created_at;
+//	모바일이라면 별도의 변수를 추가
+}
