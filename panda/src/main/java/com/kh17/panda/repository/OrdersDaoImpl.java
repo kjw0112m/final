c369558bbcf8b47ea833ec2a6db43eaa25e59556
김지원 <@222.112.120.31>
2019-08-24 23:25:11 +0900

    20190824 push

--- panda/src/main/java/com/kh17/panda/repository/OrdersDaoImpl.java
+++ panda/src/main/java/com/kh17/panda/repository/OrdersDaoImpl.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.kh17.panda.entity.OrderViewDto;
-import com.kh17.panda.vo.OrderViewListVO;
+import com.kh17.panda.entity.OrdersDto;
 import com.kh17.panda.vo.OrderViewVO;
 
 @Repository
@@ -25,10 +27,56 @@ public class OrdersDaoImpl implements OrdersDao{
 	}
 
 	@Override
-	public List<OrderViewDto> list(OrderViewDto orderViewDto, OrderViewListVO orderViewListVO) {
+	public List<OrderViewDto> list(OrderViewDto orderViewDto, List<OrderViewVO> orderViewVO, int start, int end) {
 		Map<String, Object> map = new HashMap<>();
 		map.put("dto", orderViewDto);
-		map.put("vo", orderViewListVO.getSearch());
+		map.put("vo", orderViewVO);
+		map.put("start", start);
+		map.put("end", end);
+		
 		return sqlSession.selectList("order.list", map);
+	}
+
+	@Override
+	public void insert(OrdersDto ordersDto) {
+		sqlSession.insert("order.insert", ordersDto);
+	}
+
+	@Override
+	public int seq() {
+		return sqlSession.selectOne("order.seq");
+	}
+
+	@Override
+	public void cancel(OrdersDto ordersDto) {
+		sqlSession.delete("order.cs_status", ordersDto);
+	}
+	
+	@Override
+	public void delete() {
+		sqlSession.delete("order.delete");
+	}
+
+	@Override
+	public void invoice(OrdersDto ordersDto) {
+		sqlSession.update("order.invoice", ordersDto);
+	}
+
+	@Override
+	public int count(OrderViewDto orderViewDto, List<OrderViewVO> orderViewVO) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("dto", orderViewDto);
+		map.put("vo", orderViewVO);
+		return sqlSession.selectOne("order.count", map);
+	}
+
+	@Override
+	public void stat_change(OrdersDto ordersDto) {
+		sqlSession.delete("order.status", ordersDto);
+	}
+
+	@Override
+	public List<OrderViewDto> getTeam(String team) {
+		return sqlSession.selectList("order.getTeam", team);
 	}
 }
