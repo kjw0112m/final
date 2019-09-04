1451008b1eabce63aca8379cf9cacf35da1a6bbf
dooreekim0 <dooreekim0@gmail.com>
2019-09-04 19:24:06 +0900

    190904 관리자-판매자 상품 목록 수정중

--- panda/src/main/webapp/WEB-INF/views/seller/product/list.jsp
+++ panda/src/main/webapp/WEB-INF/views/seller/product/list.jsp
@@ -1,13 +1,11 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>    
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
-</head>
-<script src="https://code.jquery.com/jquery-latest.js"></script>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<jsp:include page="/WEB-INF/views/template/admin/header.jsp"></jsp:include>
+<jsp:include page="/WEB-INF/views/template/admin/aside.jsp"></jsp:include>
+<link rel="stylesheet"
+	href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <script>
 $(function() {
 	$("#all_check").change(function() {
@@ -25,19 +23,105 @@ $(function() {
 	})
 })
 </script>
+<script src="${pageContext.request.contextPath}/js/admin/product_search.js"></script>
 <body>
+	<form action="list" method="post" id="productSearchForm">
+		<div id="content">
+			<div class="head_tit">
+				<h1>상품 목록</h1>
+			</div>
+			<div class="optionArea">
+				<div class="mOption">
+					<table border="1" summary="">
+						<colgroup>
+							<col style="width: 170px;">
+							<col style="width: 600px;">
+							<col style="width: 170px;">
+							<col style="width: auto;">
+						</colgroup>
+						<tbody>
+							<tr>
+							<th scope="row">검색분류</th>
+							<td colspan="3">
+								<div id="mainSearch">
+									<div>
+										<select class="fSelect" name="search[0].type" style="width: 163px;">
+											<option value="product_name">상품명</option>
+											<option value="product_id">상품번호</option>
+										</select>
+										<input type="text" class="fTextsearchBox" name="search[0].keyword" id="searchBox" style="width: 400px;">
+										<a href="#none" class="plus"><span class="plusbtn">+</span></a>							
+									</div>
+								</div>
+								<div id="subSearch" style="display: none">
+									<div>
+										<select class="fSelect" name="search[0].type" style="width: 163px;">
+											<option value="product_name">상품명</option>
+											<option value="product_id">상품번호</option>
+										</select>
+										<input type="text" class="fTextsearchBox" id="searchBox" style="width: 400px;">
+										<a href="#none" class="minus"><span class="minusbtn">-</span></a>
+									</div>								
+								</div>
+							</td>
+						</tr>	
+						<tr>
+							<th scope="row">상품등록일</th>
+							<td colspan="3">
+								<a href="#none" class="btnDate" date-interval="0"><span>오늘</span></a>
+								<a href="#none" class="btnDate" date-interval="1"><span>어제</span></a>
+								<a href="#none" class="btnDate" date-interval="3"><span>3일</span></a>
+								<a href="#none" class="btnDate" date-interval="7"><span>7일</span></a>
+								<a href="#none" class="btnDate" date-interval="15"><span>15일</span></a>
+								<a href="#none" class="btnDate bc_g" date-interval="30"><span>1개월</span></a>
+								<a href="#none" class="btnDate" date-interval="90"><span>3개월</span></a>
+								<a href="#none" class="btnDate" date-interval="180"><span>6개월</span></a>
+								<span class="gBreak">
+									<input type="text" id="startDate" name="start_dt" class="fText gDate" value="">
+									~
+									<input type="text" id="endDate" name="end_dt" class="fText gDate" readonly="readOnly">
+								</span>
+							</td>
+						</tr>
+<!-- 						<tr> -->
+<!-- 							<th scope="row">상품분류</th> -->
+<!-- 							<td colspan="3"> -->
+<!-- 								<select class="fSelect"  id="eCategory1" name="category"> -->
+<!-- 									<option value="" selected="">- 대분류 선택 -</option> -->
+<!-- 									<option value="1">상의</option> -->
+<!-- 									<option value="2">하의</option> -->
+<!-- 									<option value="3">악세서리</option> -->
+<!-- 								</select> -->
+<!-- 							</td> -->
+<!-- 						</tr> -->
+						</tbody>
+					</table>	
+				</div>
+				
+				<div class="form-button">
+					<input type="submit" style="display:none">
+					<a href="#none" id="btnSearch" class="btnSearch"><span>검색</span></a>
+					<a href="search" id="btnInit" class="btnSearch reset"><span>초기화</span></a>
+				</div>
+			</div>
+	</form>
+
+	
+	<div class="section" id="">
+		<div id="searchResultList" class="mBoard typeOrder gScroll gCellSingle">
 	<form action="delete" method="get">
-	<table border="1" width="800">
+	<table border="1" summary="" class="thead" width="800">
 		<thead>
 			<tr>
-				<th><input type="checkbox" id="all_check"></th>
-				<th>상품명</th>
-				<th>판매가</th>
-				<th>진열상태</th>
-				<th>판매상태</th>
-				<th>상품분류</th>
-				<th>상품 등록일</th>
-				<th>수정</th>
+				<th scope="col" class="w24"><input type="checkbox" id="all_check"></th>
+				<th scope="col" class="w60" style="display: none;">No</th>
+				<th scope="col" class="w200" style="">상품명</th>
+				<th scope="col" class="w120" style="">판매가</th>
+				<th scope="col" class="w100" style="">진열상태</th>
+				<th scope="col" class="w100" style="">판매상태</th>
+				<th scope="col" class="w200" style="">상품분류</th>
+				<th scope="col" class="w250" style="">상품 등록일</th>
+				<th scope="col" class="w100 center" style="">수정</th>
 			</tr>
 		</thead>
 		<tbody align="center">
@@ -61,5 +145,9 @@ $(function() {
 		<input type="submit" value="삭제">
 	</div>
 	</form>
+	</div>
+	</div>
+	<!-- div content 닫는 태그 -->
+	</div>
 </body>
 </html>
---
+++ panda/src/main/webapp/resources/js/admin/product_search.js
@@ -0,0 +1,342 @@
+$(function() {
+	// ----------------------------------------------- 검색어 복사 이벤트
+	// ---------------------------------------------------------
+	var subSearch = $("#subSearch").children().clone();
+	var subCount = 1;
+	$(".plus")
+			.click(
+					function() {
+						if (subCount < 5) {
+							var clone = subSearch.clone();
+							$("#mainSearch").append(clone);
+							clone.find('select').attr('name',
+									'search[' + subCount + '].type').attr(
+									'required', true);
+							clone.find('input').attr('name',
+									'search[' + subCount + '].keyword');
+							subCount += 1;
+
+							clone
+									.find(".minus")
+									.click(
+											function() {
+												$(this).parent().remove();
+												for (var i = 1; i < $(
+														'#mainSearch').find(
+														'div').length; i++) {
+													$('#mainSearch')
+															.find('select')
+															.eq(i)
+															.attr(
+																	'name',
+																	'search['
+																			+ i
+																			+ '].type')
+															.attr('required',
+																	true);
+													$('#mainSearch')
+															.find('input')
+															.eq(i)
+															.attr(
+																	'name',
+																	'search['
+																			+ i
+																			+ '].keyword');
+												}
+												subCount -= 1;
+											});
+
+						} else {
+							alert("검색어는 최대 5개까지 만들 수 있습니다.");
+						}
+					});
+
+	// ----------------------------------------------- 조건 유지 이벤트
+	// ---------------------------------------------------------
+	for (var i = 1; i < searchType.length; i++) {
+		if (searchType[i]) {
+			$(".plus").trigger("click");
+		}
+	}
+	if (searchType[0]) {
+		$("select[name='search[0].type']").find(
+				'option[value=' + searchType[0] + ']').attr('selected',
+				'selected');
+		$("select[name='search[1].type']").find(
+				'option[value=' + searchType[1] + ']').attr('selected',
+				'selected');
+		$("select[name='search[2].type']").find(
+				'option[value=' + searchType[2] + ']').attr('selected',
+				'selected');
+		$("select[name='search[3].type']").find(
+				'option[value=' + searchType[3] + ']').attr('selected',
+				'selected');
+		$("select[name='search[4].type']").find(
+				'option[value=' + searchType[4] + ']').attr('selected',
+				'selected');
+		$("input[name='search[0].keyword']").val(searchKeyword[0]);
+		$("input[name='search[1].keyword']").val(searchKeyword[1]);
+		$("input[name='search[2].keyword']").val(searchKeyword[2]);
+		$("input[name='search[3].keyword']").val(searchKeyword[3]);
+		$("input[name='search[4].keyword']").val(searchKeyword[4]);
+	}
+
+	if (productName) {
+		$("input[name='product_name']").val(productName);
+	}
+
+	// ----------------------------------------------- 체크박스 이벤트
+	// ---------------------------------------------------------
+	var tStatus = $("#tStatusCheck").find(".fChk");
+	var csStatus = $("#csStatusCheck").find(".fChk");
+	if (!page) {
+		tStatus.prop("checked", true);
+	}
+	tStatus.first().change(function() {
+		if ($("#tStatAll").is(":checked")) {
+			tStatus.prop("checked", true);
+		} else {
+			tStatus.prop("checked", false);
+		}
+	});
+
+	tStatus.not(tStatus.first()).change(function() {
+		$("#tStatAll").prop("checked", false);
+		var check = 0;
+		$(tStatus.not(tStatus.first())).each(function() {
+			if ($(this).is(':checked')) {
+				check++;
+			}
+		});
+		if (check == 5) {
+			$("#tStatAll").prop("checked", true);
+		}
+	});
+	csStatus.first().change(function() {
+		if ($("#csStatAll").is(":checked")) {
+			csStatus.prop("checked", true);
+		} else {
+			csStatus.prop("checked", false);
+		}
+	});
+	csStatus.not("#csStatAll").change(function() {
+		$("#csStatAll").prop("checked", false);
+		var check = 0;
+		$(csStatus.not('#csStatAll')).each(function() {
+			if ($(this).is(':checked')) {
+				check++;
+			}
+		});
+		if (check == 4) {
+			$("#csStatAll").prop("checked", true);
+		}
+	});
+
+	// ----------------------------------------------- 조건 유지 이벤트
+	// ---------------------------------------------------------
+
+	for (var i = 0; i < tStatusAry.length; i++) {
+		if (tStatusAry[i]) {
+			$("input[value=" + tStatusAry[i] + "]").prop('checked', true);
+		} else {
+		}
+	}
+
+	for (var i = 0; i < csStatusAry.length; i++) {
+		if (csStatusAry[i]) {
+			$("input[value=" + csStatusAry[i] + "]").prop('checked', true);
+		}
+	}
+
+	$("input[name='pay_status']").each(function() {
+		if ($(this).val() == payStatus)
+			$(this).prop('checked', true);
+	});
+
+	// 전체 체크박스 외에 모든 박스가 체크되있으면 전체 체크박스도 체크( 단, 조건 유지 이벤트 다음 실행)
+	var tcheck = 0;
+	$(tStatus.not(tStatus.first())).each(function() {
+		if ($(this).is(':checked')) {
+			tcheck++;
+		}
+	});
+	if (tcheck == 5) {
+		$("#tStatAll").prop("checked", true);
+	}
+	var cscheck = 0;
+	$(csStatus.not('#csStatAll')).each(function() {
+		if ($(this).is(':checked')) {
+			cscheck++;
+		}
+	});
+	if (cscheck == 4) {
+		$("#csStatAll").prop("checked", true);
+	}
+
+	// ----------------------------------------------- 날짜 버튼 이벤트
+	// ---------------------------------------------------------
+	var btnDate = $('.btnDate');
+	btnDate.click(function() {
+		var interval = $(this).attr('date-interval');
+		switch (interval) {
+		case '0':
+			$('#startDate').datepicker('setDate', 'today');
+			$('#endDate').datepicker('setDate', 'today');
+			$(this).addClass('bc_g');
+			btnDate.not(this).removeClass('bc_g');
+			break;
+		case '1':
+			$('#startDate').datepicker('setDate', 'today-1D');
+			$('#endDate').datepicker('setDate', 'today');
+			$(this).addClass('bc_g');
+			btnDate.not(this).removeClass('bc_g');
+			break;
+		case '3':
+			$('#startDate').datepicker('setDate', 'today-3D');
+			$('#endDate').datepicker('setDate', 'today');
+			$(this).addClass('bc_g');
+			btnDate.not(this).removeClass('bc_g');
+			break;
+		case '7':
+			$('#startDate').datepicker('setDate', 'today-7D');
+			$('#endDate').datepicker('setDate', 'today');
+			$(this).addClass('bc_g');
+			btnDate.not(this).removeClass('bc_g');
+			break;
+		case '15':
+			$('#startDate').datepicker('setDate', 'today-15D');
+			$('#endDate').datepicker('setDate', 'today');
+			$(this).addClass('bc_g');
+			btnDate.not(this).removeClass('bc_g');
+			break;
+		case '30':
+			$('#startDate').datepicker('setDate', 'today-1M');
+			$('#endDate').datepicker('setDate', 'today');
+			$(this).addClass('bc_g');
+			btnDate.not(this).removeClass('bc_g');
+			break;
+		case '90':
+			$('#startDate').datepicker('setDate', 'today-3M');
+			$('#endDate').datepicker('setDate', 'today');
+			$(this).addClass('bc_g');
+			btnDate.not(this).removeClass('bc_g');
+			break;
+		case '180':
+			$('#startDate').datepicker('setDate', 'today-6M');
+			$('#endDate').datepicker('setDate', 'today');
+			$(this).addClass('bc_g');
+			btnDate.not(this).removeClass('bc_g');
+			break;
+		}
+	});
+
+	$("#btnSearch").click(function() {
+		$("input[type='submit']").trigger("click");
+	})
+
+	$('#btnInit').click(function() {
+		location.reload();
+	})
+
+	// ----------------------------------------------- 달력 이벤트
+	// ---------------------------------------------------------
+	$.datepicker.setDefaults({
+		dateFormat : 'yy-mm-dd',
+		showOtherMonths : true // 빈 공간에 현재월의 앞뒤월의 날짜를 표시
+		,
+		showMonthAfterYear : true // 년도 먼저 나오고, 뒤에 월 표시
+		,
+		changeYear : true // 콤보박스에서 년 선택 가능
+		,
+		changeMonth : true // 콤보박스에서 월 선택 가능
+		,
+		showOn : "button",
+
+		buttonImage : rootContext + "/image/admin/order/cal.png" // 버튼 이미지 경로
+		,
+		buttonImageOnly : true // 기본 버튼의 회색 부분을 없애고, 이미지만 보이게 함
+		,
+		buttonText : "선택" // 버튼에 마우스 갖다 댔을 때 표시되는 텍스트
+		,
+		yearSuffix : "년" // 달력의 년도 부분 뒤에 붙는 텍스트
+		,
+		monthNamesShort : [ '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
+				'11', '12' ] // 달력의 월 부분 텍스트
+		,
+		monthNames : [ '1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월',
+				'10월', '11월', '12월' ] // 달력의 월 부분 Tooltip 텍스트
+		,
+		dayNamesMin : [ '일', '월', '화', '수', '목', '금', '토' ] // 달력의 요일 부분 텍스트
+		,
+		dayNames : [ '일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일' ] // 달력의
+		// 요일 부분
+		// Tooltip
+		// 텍스트
+		,
+		minDate : "-1Y" // 최소 선택일자(-1D:하루전, -1M:한달전, -1Y:일년전)
+		,
+		maxDate : "+1D" // 최대 선택일자(+1D:하루후, -1M:한달후, -1Y:일년후)
+	});
+
+	$('#startDate').datepicker();
+	$('#endDate').datepicker();
+	if (startDate) {
+		$('#startDate').datepicker('setDate', startDate);
+	} else
+		$('#startDate').datepicker('setDate', 'today-1M');
+	if (endDate) {
+		$('#endDate').datepicker('setDate', endDate);
+	} else
+		$('#endDate').datepicker('setDate', 'today');
+
+	if ($('#endDate').datepicker('setDate', 'today').val() == endDate) {
+		if ($('#startDate').datepicker('setDate', 'today').val() == startDate) {
+			$('.btnDate').eq(0).trigger('click');
+		} else if ($('#startDate').datepicker('setDate', 'today-1D').val() == startDate) {
+			$('.btnDate').eq(1).trigger('click');
+		} else if ($('#startDate').datepicker('setDate', 'today-3D').val() == startDate) {
+			$('.btnDate').eq(2).trigger('click');
+		} else if ($('#startDate').datepicker('setDate', 'today-7D').val() == startDate) {
+			$('.btnDate').eq(3).trigger('click');
+		} else if ($('#startDate').datepicker('setDate', 'today-15D').val() == startDate) {
+			$('.btnDate').eq(4).trigger('click');
+		} else if ($('#startDate').datepicker('setDate', 'today-1M').val() == startDate) {
+			$('.btnDate').eq(5).trigger('click');
+		} else if ($('#startDate').datepicker('setDate', 'today-3M').val() == startDate) {
+			$('.btnDate').eq(6).trigger('click');
+		} else if ($('#startDate').datepicker('setDate', 'today-6M').val() == startDate) {
+			$('.btnDate').eq(7).trigger('click');
+		}
+	}
+
+	// 이동 함수
+	function move(no) {
+		$("input[name=page]").val(no);
+
+		$("form").submit();
+	}
+
+	$(".page_move").click(function() {
+		var p = $(this).text();
+		move(p);
+	});
+
+	$(".page_block").click(function() {
+		var p = $(this).text();
+		switch (p) {
+		case '<':
+			move(parseInt(page) - 1);
+			break;
+		case '<<':
+			move(parseInt(startBlock) - 1);
+			break;
+		case '>':
+			move(parseInt(page) + 1);
+			break;
+		case '>>':
+			move(parseInt(endBlock) + 1);
+			break;
+		}
+	});
+
+});
